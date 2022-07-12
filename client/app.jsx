9bda9a3a02dad037a6a43bdbc49d0d8100f929cb
Brian Luong <bhluong1codes@gmail.com>
2022-07-12 05:23:54 +0000

    Added hash-routing

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import Home from './pages/home';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+
+    window.addEventListener('hashchange', () => {
+      const newHash = parseRoute(window.location.hash);
+      this.setState({ route: newHash });
+    });
+
+  }
+
   render() {
     return <Home />;
   }
